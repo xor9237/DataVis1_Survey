c31a938ea64e9801887a72acb0a4c4a9818c94b4
xor9237 <ktlee927@gmail.com>
2020-04-08 23:55:59 -0700

    commit again

---
+++ scratch_1.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+df_survey = pd.read_csv('/Users/kitaeklee/Desktop/Data/coursera_survey/Topic_Survey_Assignment.csv')
+
+
+df_survey = df_survey.sort_values(by=['Very interested'],
+                                  ascending=False)
+df_survey = df_survey.reset_index()
+i=0
+for i in df_survey.index:
+    df_survey.loc[i, 'Very interested'] = round((df_survey.loc[i, 'Very interested']/2233), 2)
+    df_survey.loc[i, 'Somewhat interested'] = round((df_survey.loc[i, 'Somewhat interested'] / 2233), 2)
+    df_survey.loc[i, 'Not interested'] = round((df_survey.loc[i, 'Not interested'] / 2233), 2)
+    i+=1
+    if i==6:
+        break
+
+print(df_survey.loc[:, ['Very interested','Somewhat interested', 'Not interested']])
